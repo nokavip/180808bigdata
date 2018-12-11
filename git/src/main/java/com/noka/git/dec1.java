5a2c34e5d8c25325c9f386163c7f9540815ef49f
tianshuishao <45785066+tianshuishao@users.noreply.github.com>
2018-12-11 20:34:18 +0800

    add shuchuhang

--- git/src/main/java/com/noka/git/dec1.java
+++ git/src/main/java/com/noka/git/dec1.java
@@ -3,5 +3,6 @@ package com.noka.git;
 public class dec1 {
     public static void main(String[] args) {
         System.out.println("ddddddddddddddd");
+        System.out.println("shaotianshui");
     }
 }
